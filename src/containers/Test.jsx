3d7a48a424e33f0e0b0b8cd907e175f33ca8a817
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-01-17 00:25:23 +0000

    Initial boilerplate commit

---
+++ src/containers/Test.jsx
@@ -0,0 +1,14 @@
+import react, {Component} from 'react'
+import axios from 'axios'
+
+export default class Test extends Component {
+
+  componentDidMount() {
+
+  }
+  render(){
+    return (
+
+    )
+  }
+}
